d994e61a7031b27eab68cf396ebb739278c80ac1
Wajdaan234 <MMuzammil2311F@aptechorangi.com>
2026-05-26 02:46:33 +0500

    ab agy  complete  work

--- src/assets/js/style.js
+++ src/assets/js/style.js
@@ -1,9 +1,9 @@
 const hamburger = document.getElementById("hamburger");
-// const navLinks = document.getElementById("navLinks");
+const navLinks = document.getElementById("navLinks");
 
-// hamburger.addEventListener("click", () => {
-//     navLinks.classList.toggle("active");
-// });
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
 
 // *****************GSAP******************
 
