9137d9b8daea23a2c67a8d6a390dacfdbe524209
MuzammilAnsari20 <MMuzammil2311F@aptechorangi.com>
2026-06-10 00:37:07 +0500

    mon

--- src/components/heroBanner.jsx
+++ src/components/heroBanner.jsx
@@ -22,35 +22,75 @@ const childVariants = {
 function HeroBanner() {
     return (
         <>
-            <section className="h-[70vh] max-md:h-[42vh] w-full box-border flex max-md:flex-col-reverse relative items-center justify-between max-md:justify-center bg-center bg-cover max-md:py-5!" style={{ backgroundImage: `url(${header_bg})` }}>
+            <section
+                className="h-[70vh] max-md:h-[45vh] w-full box-border flex max-md:flex-col-reverse relative items-center justify-between max-md:justify-center bg-center bg-cover max-md:py-5!"
+                style={{ backgroundImage: `url(${header_bg})` }}
+            >
 
-                <motion.div
-                    className="max-md:w-full max-md:h-[60%]! max-md:flex max-md:justify-center max-md:items-center"
-                    variants={containerVariants}
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: false, amount: 0.6 }}  // repeat animation, 60% visible trigger
-                >
-                    <motion.div className="ml-28! max-md:ml-0! z-10 max-md:text-center"
+                {/* Text */}
+                <div className="w-1/2 max-md:w-full flex items-center justify-center">
+                    <motion.div
+                        className="max-w-[1050px] px-5"
                         variants={containerVariants}
                         initial="hidden"
                         whileInView="visible"
                         viewport={{ once: false, amount: 0.6 }}
                     >
                         <motion.h1 variants={childVariants} className="text-white text-1xl max-md:text-[22px]! desktop-md:text-[34px] monitor-md:text-[60px] font-bold Poppins">M.Perfume</motion.h1>
+
                         <motion.h1 variants={childVariants} className="text-white text-6xl max-md:text-[32px]! desktop-md:text-[76px] monitor-md:text-[100px] font-bold Poppins"><span className="text-[#D361EB]">Collection</span> 2026</motion.h1>
+
                         <motion.h1 variants={childVariants} className="text-white text-lg max-md:text-[14px]! desktop-md:text-[34px] monitor-md:text-[60px] Poppins">Where Passion meet <span className="text-[#D361EB]">Elegance</span></motion.h1>
+
                         <motion.h6 variants={childVariants} className="text-white text-[12px] max-md:text-[12px]! desktop-md:text-[26px] monitor-md:text-[46px] Poppins">The Scent of Unforgettable <br /> Moments.</motion.h6>
                     </motion.div>
-                </motion.div>
-
-                <div className=" w-[40%] max-md:w-full h-[70%] max-md:h-[40%]! desktop-md:max-h-[50%]! monitor-md:max-h-[50%] relative flex items-center justify-center flex-col">
-                    <img src={float_image1} alt="" className="absolute w-[130px] max-md:w-[80px] desktop-md:w-[230px] monitor-md:w-[300px] left-[35%] max-md:left-[38%] top-[15%] desktop-md:top-[30%] z-1 floatimg1 animate-[floatimg1_5s_ease-in-out_infinite]" />
-                    <img src={float_image2} alt="" className="absolute w-[250px] max-md:w-[100px] desktop-md:w-[350px] monitor-md:w-[500px] top-[-15%] desktop-md:top-[0%] max-md:top-[-10%] z-2 floatimg2 animate-[floatimg2_5s_ease-in-out_infinite]" />
-                    <img src={float_image3} alt="" className="absolute w-[130px] max-md:w-[80px] desktop-md:w-[230px] monitor-md:w-[300px] right-[35%] max-md:right-[38%] top-[15%] desktop-md:top-[30%] z-1 floatimg3 animate-[floatimg3_5s_ease-in-out_infinite]" />
                 </div>
 
-            </section >
+                {/* Images */}
+                <div className="w-1/2 max-md:w-full flex items-center justify-center relative h-[500px] max-md:h-[250px]">
+
+                    <img
+                        src={float_image1}
+                        alt=""
+                        className="
+                                absolute
+                                w-[clamp(80px,13vw,320px)]
+                                monitor-md:w-[12vw]
+                                monitor-md:top-[10%]
+                                left-[35%] max-md:left-[38%]
+                                top-[30%]
+                                animate-[floatimg1_5s_ease-in-out_infinite]
+                            "
+                    />
+
+                    <img
+                        src={float_image2}
+                        alt=""
+                        className="
+                            absolute
+                            w-[clamp(150px,20vw,500px)]
+                            monitor-md:w-[20vw]
+                            z-10
+                            animate-[floatimg2_5s_ease-in-out_infinite]
+                        "
+                    />
+
+                    <img
+                        src={float_image3}
+                        alt=""
+                        className="
+                            absolute
+                            w-[clamp(80px,13vw,320px)]
+                            monitor-md:w-[12vw]
+                            monitor-md:top-[10%]
+                            right-[35%] max-md:right-[38%]
+                            top-[30%]
+                            animate-[floatimg3_5s_ease-in-out_infinite]
+                        "
+                    />
+                </div>
+
+            </section>
         </>
     );
 }
